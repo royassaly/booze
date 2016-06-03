2fe7e70c7b8c437cd4f2c2f47aef245d4f3646dd
Roy Abou Assaly <royassaly@gmail.com>
2016-06-02 20:05:58 -0400

    Add files via upload

---
+++ booze/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BoozeItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    link = scrapy.Field()
+    price = scrapy.Field()
+    # TODO, add UPC, Alcohol %, and size of bottle in mL
+    # upc = scrapy.Field()
