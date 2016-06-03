bbfb1d0544e065c22648f2175c9726551b1d0f60
Roy Abou Assaly <royassaly@gmail.com>
2016-06-02 20:50:12 -0400

    More path fixes.

--- runScotch.sh
+++ runScotch.sh
@@ -1,11 +1,11 @@
 #/bin/sh
+rm scotch.csv
+cd ./booze
 rm saq.csv
 rm lcbo.csv
 rm saqnoheader.csv
-cd ./booze
 scrapy crawl lcbo -o lcbo.csv
 scrapy crawl saq -o saq.csv
 sed 1d saq.csv > saqnoheader.csv
 cat lcbo.csv saqnoheader.csv > scotch.csv
-
-
+mv scotch.csv ../
