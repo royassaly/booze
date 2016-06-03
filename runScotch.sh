44def89090c29f91a56062963542f89bc678b148
Roy Abou Assaly <royassaly@gmail.com>
2016-06-02 20:34:03 -0400

    fix path.
    Getting things synced to my laptop for ease of updating.

--- runScotch.sh
+++ runScotch.sh
@@ -2,7 +2,7 @@
 rm saq.csv
 rm lcbo.csv
 rm saqnoheader.csv
-cd /Users/roy/Google\ Drive/code/scraping/booze
+cd /Users/roy/code/scraping/booze
 scrapy crawl lcbo -o lcbo.csv
 scrapy crawl saq -o saq.csv
 sed 1d saq.csv > saqnoheader.csv
