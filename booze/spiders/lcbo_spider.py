20f3317b3ee805ef49f9d6f70e409cd90526d587
Roy Abou Assaly <royassaly@gmail.com>
2019-09-13 14:11:04 -0400

    FIX: LCBO changed all their HTML
    Updated the basic fields to work again against the new HTML. Still need to fix a few more fields.

--- booze/spiders/lcbo_spider.py
+++ booze/spiders/lcbo_spider.py
@@ -6,23 +6,27 @@ from booze.items import BoozeItem
 class LcboSpider(scrapy.Spider):
     name = "lcbo"
     allowed_domains = ["lcbo.com"]
-    # Scotch, Scotch Whisky Blend = [11086, 11083], All Scotch and Whisky = [11014]
-    categories = [11014]
-    # Set max value to 66 since there are 66 pages
     start_urls = [
-      # FOR TESTING:
-      #'http://www.lcbo.com/lcbo/catalog/scotch-single-malts/{}?contentBeginIndex=0&productBeginIndex=%s&beginIndex=%s&orderBy=&categoryPath=spirits/whisky-whiskey/scotch-single-malts&orderByContent=&facet=&storeId=10151&catalogId=10001&langId=-1&requesttype=ajax'.format(c) %(n*12, n*12) for n in range(0, 12) for c in categories
-      'http://www.lcbo.com/lcbo/catalog/scotch-single-malts/{}?contentBeginIndex=0&productBeginIndex=%s&beginIndex=%s&orderBy=&categoryPath=spirits/whisky-whiskey/scotch-single-malts&orderByContent=&facet=&storeId=10151&catalogId=10001&langId=-1&requesttype=ajax'.format(c) %(n*12, n*12) for n in range(0, 66) for c in categories
-    ]
-    
+      # URLs to scrape
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=0",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=100",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=200",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=300",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=400",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=500",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=600",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=700",
+     "https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15/spirits-15/whisky-whiskey-15020?pageView=grid&orderBy=1&fromPage=catalogEntryList&pageSize=100&beginIndex=800"
+ ]
+  
     custom_settings = {
     # specifies exported fields and order
     'FEED_EXPORT_FIELDS': ["title", "alcohol", "volume", "sale", "savings", "price", "inventory", "link"],
     }
 
     def parse(self, response):
-        for href in response.xpath("//div[@class='product-wrapper']/div[@class='product-name']/a/@href"):
-            url = 'http://www.lcbo.com' + href.extract()
+        for href in response.xpath("//div[@class='productChart']/div[@class='product_name']/a/@href"):
+            url = href.extract()
             yield scrapy.Request(url, callback=self.parse_dir_contents)
 
     def parse_dir_contents(self, response):
@@ -31,22 +35,23 @@ class LcboSpider(scrapy.Spider):
         price = 0
         rangeVolumeBottle = len(" bottle")
         rangeVolumeGift = len(" gift")
+        item['title'] = ''
         item['sale'] = ''
         item['savings'] = ''
         item['alcohol'] = ''
         item['inventory'] = ''
         savings = 0
-        checkInventoryURL = ''
-        inventoryPage = []
+        #checkInventoryURL = ''
+        #inventoryPage = []
         
-        item['title'] = response.xpath("//div/h1/text()").extract()[0].strip()
+        item['title'] = response.xpath("//div/h1[@role='heading']/text()").extract()[0].strip()
         item['link'] = response.url
-        item['price'] = response.xpath("//span[@class='price-value']/text()").extract()[0].strip()
+        item['price'] = response.xpath("//span[@class='price']/text()").extract()[0].strip()
         savings = response.xpath("//small[@class='saving']/text()").extract()
         if len(savings) == 2:
             item['sale'] = "On sale!"
             item['savings'] = savings[1]
-        volume = response.xpath("//dt[@class='product-volume']/text()").extract()
+        volume = response.xpath("//div[@class='product-details-list']/div/b[text() = 'Bottle Size:']/following-sibling::span[1]/text()").extract()[0].strip()
         item['volume'] = volume
         
         # u'750 mL'
@@ -55,18 +60,19 @@ class LcboSpider(scrapy.Spider):
           item['volume'] = [elem[:len(elem) - rangeVolumeGift] for elem in volume]
           
         # Get the alcohol % from the product details section 
-        alcohol = response.xpath("//dt[text() = 'Alcohol/Vol']/following-sibling::dd[1]/text()").extract()
+        alcohol = response.xpath("//div[@class='product-details-list']/div/b[text() = 'Alcohol/Vol:']/following-sibling::span[1]/text()").extract()[0].strip()
         item['alcohol'] = alcohol
-        
+
+        # Rewrite all of the code below. HTML has changed again on the LCBO website.
+
         # Check if the item if even available, if not: Sorry, the product you selected is currently not available in any store.
-        checkInventoryURL = response.xpath("//button[@id='check-store-inventory']/@data-modal-content").extract()[0].strip()
-        request =  scrapy.Request(str(checkInventoryURL),callback=self.inventory_parse)
-        request.meta['item'] = item     
-        yield request
+        #checkInventoryURL = response.xpath("//button[@id='check-store-inventory']/@data-modal-content").extract()[0].strip()
+        #request =  scrapy.Request(str(checkInventoryURL),callback=self.inventory_parse)
+        #request.meta['item'] = item     
+        #yield request
            
-    def inventory_parse(self,response):
-        item = BoozeItem()
-        item = response.meta['item']
-        item['inventory'] = response.xpath("//td[@class='no-inventory']/text()").extract()
+  #  def inventory_parse(self,response):
+   #     item = BoozeItem()
+   #     item = response.meta['item']
+  #      item['inventory'] = '' # response.xpath("//td[@class='no-inventory']/text()").extract()
         yield item
-        
